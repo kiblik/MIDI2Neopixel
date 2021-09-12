5ac8763badc88ea8de2d938ab46953c6d9b4f882
Tomas Kubla <tomas@kubla.sk>
2021-09-12 20:56:26 +0000

    save memory (drop autoconfig, add ram monitor) + fix types handling + use native timers

--- Software/MIDI2Neopixel/MIDI2Neopixel.ino
+++ Software/MIDI2Neopixel/MIDI2Neopixel.ino
@@ -1,8 +1,6 @@
-// Developed in Arduino IDE 1.8.13 (Windows)
-
-#include <FastLED.h> // FastLED version 3.3.3 https://github.com/FastLED/FastLED
-#include "MIDIUSB.h" // MIDIUSB version 1.0.4 https://www.arduino.cc/en/Reference/MIDIUSB
-#include <arduino-timer.h>
+#include <FastLED.h> // FastLED version 3.4.0 https://github.com/FastLED/FastLED
+#include <MIDIUSB.h> // MIDIUSB version 1.0.5 https://www.arduino.cc/en/Reference/MIDIUSB
+#include <timer-api.h> // arduino-timer-api version 0.1.0 https://github.com/sadr0b0t/arduino-timer-api
 
 #define BUTTON_PIN1 3
 #define BUTTON_PIN2 2
@@ -15,12 +13,11 @@ uint16_t NUM_LEDS = DEFAULT_LEDS;
 #define COLOR_ORDER GRB
 #define AMPERAGE    10000     // Power supply amperage in mA
 
-#define PR_AUTOLEARN 0
-#define PR_RGB_ACYC 1
-#define PR_RGB_CYC 2
+#define PR_RGB_ACYC 0
+#define PR_RGB_CYC 1
 #define PR_DEFAULT PR_RGB_ACYC
 
-#define MAX_PROGRAM 2 // 0..2
+#define MAX_PROGRAM 1 // 0..1
 #define LED_INT_STEPS 8      // LED Intensity button steps
 //#define DEBUG
 
@@ -28,50 +25,28 @@ CRGB leds[MAX_LEDS];
 byte program = PR_DEFAULT;
 #define ledIntensity LED_INT_STEPS // default LED brightness
 byte ledBrightness = ceil(255 / LED_INT_STEPS * ledIntensity); // default LED brightness
-byte FPS = 12;
+uint8_t FPS = 30;
 
-Timer<1> timer;
 bool enableRender = false;
-uint16_t processedMsgs = 1; // needs to be 1 for first render
+uint8_t processedMsgs = 1; // needs to be 1 for first render
 
 //------------------------------------------- MAPPING ----------------------------------------------//
 
-#define CC_DEFAULT_R 0x11
-#define CC_DEFAULT_G 0x12
-#define CC_DEFAULT_B 0x13
-#define CC_DEFAULT_INTENSITY 0x14
-#define CC_DEFAULT_POSITION 0x09
-#define CC_DEFAULT_SIZE 0x02
-#define CC_DEFAULT_FADEIN 0x03
-#define CC_DEFAULT_FADEOUT 0x04
-#define CC_DEFAULT_BASE_R 0x15
-#define CC_DEFAULT_BASE_G 0x16
-#define CC_DEFAULT_BASE_B 0x17
-#define CC_DEFAULT_BASE_INTENSITY 0x18
-#define CC_DEFAULT_FPS 0x05
-#define CC_DEFAULT_LENGTH_LOW 0x06
-#define CC_DEFAULT_LENGTH_HIGH 0x07
-
-uint8_t CC_R = CC_DEFAULT_R;
-uint8_t CC_G = CC_DEFAULT_G;
-uint8_t CC_B = CC_DEFAULT_B;
-uint8_t CC_INTENSITY = CC_DEFAULT_INTENSITY;
-uint8_t CC_POSITION = CC_DEFAULT_POSITION;
-uint8_t CC_SIZE = CC_DEFAULT_SIZE;
-uint8_t CC_FADEIN = CC_DEFAULT_FADEIN;
-uint8_t CC_FADEOUT = CC_DEFAULT_FADEOUT;
-uint8_t CC_BASE_R = CC_DEFAULT_BASE_R;
-uint8_t CC_BASE_G = CC_DEFAULT_BASE_G;
-uint8_t CC_BASE_B = CC_DEFAULT_BASE_B;
-uint8_t CC_BASE_INTENSITY = CC_DEFAULT_BASE_INTENSITY;
-uint8_t CC_FPS = CC_DEFAULT_FPS;
-uint8_t CC_LENGTH_LOW = CC_DEFAULT_LENGTH_LOW;
-uint8_t CC_LENGTH_HIGH = CC_DEFAULT_LENGTH_HIGH;
-
-#define MAX_CONFIG 14 // 0..14
-#define OFFSET_CONFIG 0
-
-byte config_id = 0;
+#define CC_R 0x11
+#define CC_G 0x12
+#define CC_B 0x13
+#define CC_INTENSITY 0x14
+#define CC_POSITION 0x09
+#define CC_SIZE 0x02
+#define CC_FADEIN 0x03
+#define CC_FADEOUT 0x04
+#define CC_BASE_R 0x15
+#define CC_BASE_G 0x16
+#define CC_BASE_B 0x17
+#define CC_BASE_INTENSITY 0x18
+#define CC_FPS 0x05
+#define CC_LENGTH_LOW 0x06
+#define CC_LENGTH_HIGH 0x07
 
 typedef struct { 
   uint8_t r = 255;
@@ -93,88 +68,13 @@ uint8_t base_g = 0;
 uint8_t base_b = 0;
 uint8_t base_intensity = 0;
 
-
-//------------------------------------------- CONFIG ----------------------------------------------//
-void showConfig(byte value){
-  for (uint16_t i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Black;
-  }
-  leds[(config_id % (MAX_CONFIG+1)) + OFFSET_CONFIG].setRGB(value, value, value);
+#if defined(DEBUG)
+int freeRam () {
+  extern int __heap_start, *__brkval;
+  int v;
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
-void updateConfig( byte control, byte value){
-  switch (config_id) {
-    case 0:
-      CC_R = control;
-      break;
-    case 1:
-      CC_G = control;
-      break;
-    case 2:
-      CC_B = control;
-      break;
-    case 3:
-      CC_INTENSITY = control;
-      break;
-    case 4:
-      CC_POSITION = control;
-      break;
-    case 5:
-      CC_SIZE = control;
-      break;
-    case 6:
-      CC_FADEIN = control;
-      break;
-    case 7:
-      CC_FADEOUT = control;
-      break;
-    case 8:
-      CC_BASE_R = control;
-      break;
-    case 9:
-      CC_BASE_G = control;
-      break;
-    case 10:
-      CC_BASE_B = control;
-      break;
-    case 11:
-      CC_BASE_INTENSITY = control;
-      break;
-    case 12:
-      CC_FPS = control;
-      break;
-    case 13:
-      CC_LENGTH_LOW = control;
-      break;
-    case 14:
-      CC_LENGTH_HIGH = control;
-      break;
-    default:
-      #if defined(DEBUG)
-        Serial.print(F("Control change: control="));
-        Serial.print(control, HEX);
-        Serial.print(F(", value="));
-        Serial.println(value, HEX);
-      #endif
-      return;
-  }  
-  showConfig(value);
-}
-void setConfig(byte newConfig){
-  config_id = newConfig;
-  #if defined(DEBUG)
-    Serial.print(F("Setting configuration for "));
-    Serial.println(String(config_id)); 
-  #endif
-  showConfig(128);
-}
-void nextConfig(){  
-  if ( config_id >= MAX_CONFIG ){
-    setConfig(0);
-  }
-  else {
-    setConfig(config_id + 1);
-  }
-}
+#endif
 
 //------------------------------------------- PROGRAM ----------------------------------------------//
 void showProgram(){
@@ -189,14 +89,8 @@ void setProgram(byte newProgram){
     Serial.print(F("Program is "));
     Serial.println(String(program));
   #endif
-  if (program == PR_AUTOLEARN) {
-    showProgram();
-    delay(100);
-    setConfig(0);
-  } else {
-    showProgram();
-    delay(100);
-  }
+  showProgram();
+  delay(100);
 }
 void nextProgram(){  
   if ( program >= MAX_PROGRAM ){
@@ -236,134 +130,177 @@ void update(){
     if (point.size > 0) {
       //fade in
       int j = point.fadein + 1;
-      for (int i = max(0, point.position - point.size/2 - point.fadein); i < min(NUM_LEDS, (int)(point.position - point.size/2)); i++){
+      for (int i = max(0, (int)point.position - point.size/2 - point.fadein); i < min((int) NUM_LEDS, (int)point.position - point.size/2); i++){
+        #if defined(DEBUG)
+            Serial.print(F("FadeIn  "));
+            Serial.print(i);
+        #endif
+        #if defined(DEBUG)
+          Serial.print(F("Free RAM: "));
+          Serial.print(freeRam());
+        #endif
         if ( program == PR_RGB_ACYC )
           if ( ( i < 0 ) || ( i >= (int)NUM_LEDS ) )
             continue;
         leds[i % NUM_LEDS] = setPixel( leds[i % NUM_LEDS], point, (double) 1 / (j*j) );
         j--;
+        #if defined(DEBUG)
+            Serial.println();
+        #endif
       }
       // core
-      for (int i = max(0, point.position - point.size/2); i < min(NUM_LEDS, (int)(point.position + (point.size+1)/2)); i++){
+      for (int i = max(0, (int)point.position - point.size/2); i < min((int) NUM_LEDS, (int)point.position + (point.size+1)/2); i++){
+        #if defined(DEBUG)
+            Serial.print(F("Core    "));
+            Serial.print(i);
+        #endif
+        #if defined(DEBUG)
+          Serial.print(F("Free RAM: "));
+          Serial.print(freeRam());
+        #endif
         if ( program == PR_RGB_ACYC )
           if ( ( i < 0 ) || ( i >= (int)NUM_LEDS ) )
             continue;
         leds[i % NUM_LEDS] = setPixel( leds[i % NUM_LEDS], point );
+        #if defined(DEBUG)
+            Serial.println();
+        #endif
       }
       // fade out
       j = 1;
-      for (int i = max(0, point.position + (point.size+1)/2); i < min(NUM_LEDS, (int)(point.position + (point.size+1)/2 + point.fadeout)); i++){
+      for (int i = max(0, (int)point.position + (point.size+1)/2); i < min((int) NUM_LEDS, (int)point.position + (point.size+1)/2 + point.fadeout); i++){
+        #if defined(DEBUG)
+            Serial.print(F("FadeOut "));
+            Serial.print(i);
+        #endif
+        #if defined(DEBUG)
+          Serial.print(F("Free RAM: "));
+          Serial.print(freeRam());
+        #endif
         j++;
         if ( program == PR_RGB_ACYC )
           if ( ( i < 0 ) || ( i >= (int)NUM_LEDS ) )
             continue;
         leds[i % NUM_LEDS] = setPixel( leds[i % NUM_LEDS], point, (double) 1 / (j*j) );
+        #if defined(DEBUG)
+            Serial.println();
+        #endif
       }
     }
   }
-
 }
 
 void controlChange(byte channel, byte control, byte value) {
 
-  if (program == PR_AUTOLEARN) {
-    updateConfig(control, value*2 );
-    return;
-  } else {
-    if (control == CC_R) 
-      points[channel].r = value * 2;
-    else if ( control == CC_G )
-      points[channel].g = value * 2;
-    else if ( control == CC_B )
-      points[channel].b = value * 2;
-    else if ( control == CC_INTENSITY )
-      points[channel].intensity = value * 2;
-    else if ( control == CC_POSITION ) {
-      if ( program == PR_RGB_ACYC )
-        points[channel].position = map( value, 0, 127, 0, NUM_LEDS-1);
-      else if ( program == PR_RGB_CYC )
-        points[channel].position = value % NUM_LEDS;
-    }
-    else if ( control == CC_SIZE )
-      points[channel].size = min(NUM_LEDS, value);
-    else if ( control == CC_FADEIN )
-      points[channel].fadein = min(NUM_LEDS/3, value);
-    else if ( control == CC_FADEOUT )
-      points[channel].fadeout = min(NUM_LEDS/3, value);
-    else if ( control == CC_BASE_R ) 
-      base_r = value * 2;
-    else if ( control == CC_BASE_G )
-      base_g = value * 2;
-    else if ( control == CC_BASE_B )
-      base_b = value * 2;
-    else if ( control == CC_BASE_INTENSITY )
-      base_intensity = value * 2;
-    else if ( control == CC_FPS ){
-      FPS = min(2500, max(1000/50, value));
-      timer.cancel();
-      initRender();
-    }
-    else if ( (control == CC_LENGTH_LOW) || (control == CC_LENGTH_HIGH) ){
-      uint16_t new_NUM_LEDS;
-      if ( control == CC_LENGTH_LOW )
-        new_NUM_LEDS = (NUM_LEDS & 0xFF80) | (value & 0x007F);      
-      else
-        new_NUM_LEDS = (NUM_LEDS & 0x007F) | ( (value << 7) & 0xFF80);
-      if (( 0 < new_NUM_LEDS ) && ( new_NUM_LEDS <= MAX_LEDS )){
-        enableRender = false;
-        for (uint16_t i = 0; i < NUM_LEDS; i++) {
-          leds[i] = CRGB::Black;
-        }
-        FastLED.show();
-        NUM_LEDS = new_NUM_LEDS;
-        #if defined(DEBUG)
-          Serial.print(F("New NUM_LEDS: "));
-          Serial.println(NUM_LEDS); 
-        #endif  
-        initLeds();
+  if (control == CC_R) 
+    points[channel].r = value * 2;
+  else if ( control == CC_G )
+    points[channel].g = value * 2;
+  else if ( control == CC_B )
+    points[channel].b = value * 2;
+  else if ( control == CC_INTENSITY )
+    points[channel].intensity = value * 2;
+  else if ( control == CC_POSITION ) {
+    if ( program == PR_RGB_ACYC )
+      points[channel].position = map( value, 0, 127, 0, NUM_LEDS-1);
+    else if ( program == PR_RGB_CYC )
+      points[channel].position = value % NUM_LEDS;
+  }
+  else if ( control == CC_SIZE )
+    points[channel].size = min(NUM_LEDS, value);
+  else if ( control == CC_FADEIN )
+    points[channel].fadein = min(NUM_LEDS/3, value);
+  else if ( control == CC_FADEOUT )
+    points[channel].fadeout = min(NUM_LEDS/3, value);
+  else if ( control == CC_BASE_R ) 
+    base_r = value * 2;
+  else if ( control == CC_BASE_G )
+    base_g = value * 2;
+  else if ( control == CC_BASE_B )
+    base_b = value * 2;
+  else if ( control == CC_BASE_INTENSITY )
+    base_intensity = value * 2;
+  else if ( control == CC_FPS ){
+    FPS = max(1, value);
+    timer_stop_ISR(_TIMER1);
+    initRender();
+  }
+  else if ( (control == CC_LENGTH_LOW) || (control == CC_LENGTH_HIGH) ){
+    uint16_t new_NUM_LEDS;
+    if ( control == CC_LENGTH_LOW )
+      new_NUM_LEDS = (NUM_LEDS & 0xFF80) | (value & 0x007F);      
+    else
+      new_NUM_LEDS = (NUM_LEDS & 0x007F) | ( (value << 7) & 0xFF80);
+    if (( 0 < new_NUM_LEDS ) && ( new_NUM_LEDS <= MAX_LEDS )){
+      enableRender = false;
+      for (uint16_t i = 0; i < NUM_LEDS; i++) {
+        leds[i] = CRGB::Black;
       }
-    }
-    else {
+      FastLED.show();
+      NUM_LEDS = new_NUM_LEDS;
       #if defined(DEBUG)
-        Serial.print(F("Control change: control="));
-        Serial.print(control, HEX);
-        Serial.print(F(", value="));
-        Serial.print(value, HEX);
-        Serial.print(F(", channel="));
-        Serial.println(channel);
-      #endif
-      return;
+        Serial.print(F("New NUM_LEDS: "));
+        Serial.println(NUM_LEDS); 
+      #endif  
+      initLeds();
     }
-
-    processedMsgs++;
-
+  }
+  else {
     #if defined(DEBUG)
-      Serial.print("R=");
-      Serial.print(points[channel].r, HEX);
-      Serial.print(", G=");
-      Serial.print(points[channel].g, HEX);
-      Serial.print(", B=");
-      Serial.print(points[channel].b, HEX);
-      Serial.print(F(", intensity="));
-      Serial.print(points[channel].intensity, HEX);
-      Serial.print(F(", position="));
-      Serial.print(points[channel].position, HEX);
-      Serial.print(F(", size="));
-      Serial.print(points[channel].size, HEX);
-      Serial.print(F(", fadein="));
-      Serial.print(points[channel].fadein, HEX);
-      Serial.print(F(", fadeout="));
-      Serial.print(points[channel].fadeout, HEX);
+      Serial.print(F("Control change: control="));
+      Serial.print(control, HEX);
+      Serial.print(F(", value="));
+      Serial.print(value, HEX);
       Serial.print(F(", channel="));
       Serial.println(channel);
     #endif
+    return;
   }
+
+  #if defined(DEBUG)
+    Serial.print(F("R="));
+    Serial.print(points[channel].r, HEX);
+    Serial.print(F(", G="));
+    Serial.print(points[channel].g, HEX);
+    Serial.print(F(", B="));
+    Serial.print(points[channel].b, HEX);
+    Serial.print(F(", intensity="));
+    Serial.print(points[channel].intensity);
+    Serial.print(F(", position="));
+    Serial.print(points[channel].position);
+    Serial.print(F(", size="));
+    Serial.print(points[channel].size);
+    Serial.print(F(", fadein="));
+    Serial.print(points[channel].fadein);
+    Serial.print(F(", fadeout="));
+    Serial.print(points[channel].fadeout, HEX);
+    Serial.print(F(", channel="));
+    Serial.print(channel);
+    Serial.print(F(", bR="));
+    Serial.print(base_r, HEX);
+    Serial.print(F(", bG="));
+    Serial.print(base_g, HEX);
+    Serial.print(F(", bB="));
+    Serial.print(base_b, HEX);
+    Serial.print(F(", bInt="));
+    Serial.print(base_intensity);
+    Serial.print(F(", FPS="));
+    Serial.print(FPS);
+    Serial.print(F(", NUM_LEDS="));
+    Serial.print(NUM_LEDS);
+    Serial.print(F(", procMsg="));
+    Serial.print(processedMsgs);
+    Serial.println();
+  #endif
+
+  processedMsgs++;
+
 }
 
 //-------------------------------------------- RENDER ---------------------------------------------//
 
-bool render(__attribute__((unused)) void *argument) {
-  if ((program != PR_AUTOLEARN) && (enableRender) && (processedMsgs > 0)) {
+void timer_handle_interrupts(__attribute__((unused)) int timer)  {
+  if ((enableRender) && (processedMsgs > 0)) {
     update();
     #if defined(DEBUG)
       Serial.println(F("Render"));
@@ -371,17 +308,22 @@ bool render(__attribute__((unused)) void *argument) {
     FastLED.show();
     processedMsgs = 0;
   }
-  
-  return true;
 }
 
 void initRender() {
-  uint16_t time = 1000 / FPS;
-    #if defined(DEBUG)
-      Serial.print(F("Render time: "));
-      Serial.println(time);
-    #endif
-  timer.every( time, render);
+  uint16_t trigger = min(max((uint16_t) ( ((F_CPU / 256) / FPS) - 1), 1), 65535);
+  #if defined(DEBUG)
+    uint16_t time = 1000 / FPS;
+    Serial.print(F("F_CPU: "));
+    Serial.print(F_CPU);
+    Serial.print(F(", FPS: "));
+    Serial.print(FPS);
+    Serial.print(F(", render time (ms): "));
+    Serial.print(time);
+    Serial.print(F(", trigger: "));
+    Serial.println(trigger);
+  #endif
+  timer_init_ISR(_TIMER1, TIMER_PRESCALER_1_256, trigger);
 }
 
 void initLeds() {
@@ -422,12 +364,7 @@ void setup() {
     nextProgram();
     delay(1000);
   }, RISING);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN2), [](){
-    if (program == PR_AUTOLEARN) { 
-      nextConfig();
-    }
-    delay(1000);
-  }, RISING);
+//  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN2), [](){}, RISING);
   #if defined(DEBUG)
     Serial.println(F("MIDI2Neopixel is ready!"));
   #endif
@@ -435,15 +372,10 @@ void setup() {
 
 //--------------------------------------------- LOOP ----------------------------------------------//
 void loop() {
-  timer.tick();
-
   ////////////////////////////////////////// MIDI Read routine ////////////////////////////////////////
   midiEventPacket_t rx = MidiUSB.read();
-  if (rx.header) {digitalWrite(LED_ACT_PIN, HIGH);}
+  digitalWrite(LED_ACT_PIN, HIGH);
   switch (rx.header) {
-    case 0:
-      break; //No pending events
-    
     case 0xB:
       controlChange(
         rx.byte1 & 0xF,  //channel
@@ -455,12 +387,6 @@ void loop() {
     case 0xC:
       setProgram(rx.byte2);
       break;
-
-    case 0x8: // Note off
-    case 0x9: // Note on
-    case 0xA: // Polyphonic key Pressure
-    case 0xD: // Channel Pressure
-      break;  // we are ignoring all key and pressure messages
 
     #if defined(DEBUG)
     default:
@@ -474,7 +400,6 @@ void loop() {
         Serial.println(rx.byte3, HEX);
     #endif
   }
-
-  if (rx.header) {digitalWrite(LED_ACT_PIN, LOW);}
+  digitalWrite(LED_ACT_PIN, LOW);
 }
 //------------------------------------------ END OF LOOP -------------------------------------------//
